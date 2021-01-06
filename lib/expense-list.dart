e98eaca2f627cf6cd10b30e46967efa7309b5407
Laurent Muller <loranm@protonmail.com>
2021-01-06 22:40:12 +0100

    1_first_step_to_expenses_app

---
+++ lib/expense-list.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_expenses/transaction.dart';
+
+class ExpenseList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  ExpenseList( {this.transactions});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: transactions
+            .map((transaction) => Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 5)),
+                        child:
+                        Text(
+                          transaction.amount.toString(),
+                          style: TextStyle(color: Colors.purple,fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                          Text(transaction.title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          Text(transaction.date.toString(), style: TextStyle(color: Colors.grey),)
+                        ],),
+                      )
+                    ],
+                  ),
+                ))
+            .toList());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expenses/expense-list.dart';
+import './transaction.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1', title: "New Shoes", amount: 100, date: DateTime.now()),
+    Transaction(
+      id: 't2', title: "Weekly Groceries", amount: 13.44, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                // width: double.infinity,
+                child: Card(
+                  elevation: 5,
+                  color: Colors.blue,
+                  child: Text("charts"),
+                ),
+              ),
+              ExpenseList(transactions: this.transactions)
+            ]));
+  }
+}

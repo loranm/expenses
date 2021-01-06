ef9357160dea5d2029c2b8b4121a8a875b4dcb71
Laurent Muller <loranm@protonmail.com>
2021-01-06 23:20:51 +0100

    main: Added date formatting

--- lib/expense-list.dart
+++ lib/expense-list.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_expenses/transaction.dart';
+import "package:intl/intl.dart";
 
 class ExpenseList extends StatelessWidget {
   final List<Transaction> transactions;
 
-  ExpenseList( {this.transactions});
+  ExpenseList({this.transactions});
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +18,30 @@ class ExpenseList extends StatelessWidget {
                       Container(
                         margin: EdgeInsets.all(10),
                         padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 5)),
-                        child:
-                        Text(
-                          transaction.amount.toString(),
-                          style: TextStyle(color: Colors.purple,fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 5)),
+                        child: Text(
+                          '\$${transaction.amount}',
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
                         ),
+                      ),
                       Container(
                         margin: EdgeInsets.all(10),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                          Text(transaction.title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                          Text(transaction.date.toString(), style: TextStyle(color: Colors.grey),)
-                        ],),
+                            Text(transaction.title,
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                            Text(
+                             DateFormat.yMMMd().format(transaction.date),
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
                       )
                     ],
                   ),
